172acde00dbc19e7dd729a98777c5a2330449251
Andrew <sobakasu@gmail.com>
2011-05-24 21:27:35 +0930

    removed javadocs

--- src/au/com/team2moro/couchdbsyncer/package-info.java
+++ src/au/com/team2moro/couchdbsyncer/package-info.java
@@ -4,6 +4,8 @@
  * DatabaseStore handles the storage of documents and attachments from one or more CouchDB databases.  Applications will typically create a single shared instance of DatabaseStore.
  * a Syncer object is used to synchronize a local database with a remote CouchDB database.
  * SyncerService can be subclassed to provide a IntentService to run a syncer update.
+ * 
+ * @author Andrew Williams (sobakasu@gmail.com)
  */
 
 package au.com.team2moro.couchdbsyncer;
